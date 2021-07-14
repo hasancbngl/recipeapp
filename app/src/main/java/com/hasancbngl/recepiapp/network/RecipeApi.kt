a3c187ab6167cae77e944e5a2bd588960a83e0ac
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-07-14 21:32:23 +0300

    create recipe api

---
+++ app/src/main/java/com/hasancbngl/recepiapp/network/RecipeApi.kt
@@ -0,0 +1,11 @@
+package com.hasancbngl.recepiapp.network
+
+import com.hasancbngl.recepiapp.model.Recipe
+import io.reactivex.rxjava3.core.Observable
+import retrofit2.http.GET
+import java.util.*
+
+interface RecipeApi {
+    @GET("/recipes.json")
+    fun allRecipes(): Observable<List<Recipe>>
+}

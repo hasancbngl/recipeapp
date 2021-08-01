f551511108f683417dcb6e2c3119ce6feec7ce98
Hasan Cobanoglu <hasan.cobanoglu1@outlook.com.tr>
2021-08-01 13:32:36 +0300

    add recipes fragment

--- app/src/main/java/com/hasancbngl/recepiapp/MainActivity.kt
+++ app/src/main/java/com/hasancbngl/recepiapp/MainActivity.kt
@@ -7,22 +7,24 @@ import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import com.hasancbngl.recepiapp.fragments.RecipesFragment
 import com.hasancbngl.recepiapp.model.Recipe
 import com.hasancbngl.recepiapp.mvvm.RecipeViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
-    private val viewModel : RecipeViewModel by viewModels()
+    private val viewModel: RecipeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel.list.observe(this, { recipes -> Log.i(TAG, "onCreate: $recipes") })
         viewModel.getRecipes()
 
         //todo add hilt dependency injection
         //recyclerview and update UI
+        val fragment = RecipesFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/hasancbngl/recepiapp/fragments/RecipesFragment.kt
@@ -0,0 +1,32 @@
+package com.hasancbngl.recepiapp.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.hasancbngl.recepiapp.R
+import com.hasancbngl.recepiapp.mvvm.RecipeViewModel
+
+class RecipesFragment : Fragment() {
+
+    private val recipeViewModel: RecipeViewModel by activityViewModels()
+    private val TAG = "RecipesFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recipeViewModel.list.observe(
+            requireActivity(),
+            { recipes -> Log.i(TAG, "onViewCreated: $recipes") })
+    }
+}
